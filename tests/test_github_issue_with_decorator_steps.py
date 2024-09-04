8872e897d8f95cf0cdd2804afe9c6f5369672c53
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-09-04 22:20:17 +0300

    add test with decorator steps

---
+++ tests/test_github_issue_with_decorator_steps.py
@@ -0,0 +1,36 @@
+import allure
+from selene import browser, have, be
+
+
+def test_github_issue():
+    open_main_page()
+    look_for_repo('KseniaPanfilova/QA_GURU_HW_8')
+    open_repo_by_link('/KseniaPanfilova/QA_GURU_HW_8')
+    open_issues_tab()
+    should_see_issue_with_number(1)
+
+
+@allure.step('Open GitHub main page')
+def open_main_page():
+    browser.open('https://github.com')
+
+
+@allure.step('Look for repository')
+def look_for_repo(repo):
+    browser.element('.header-search-button').click()
+    browser.element('#query-builder-test').type(repo).press_enter()
+
+
+@allure.step('Open repository\'s page by link from search result')
+def open_repo_by_link(repo):
+    browser.element(f'[href="{repo}"]').click()
+
+
+@allure.step('Go to issues tab')
+def open_issues_tab():
+    browser.element('#issues-tab').click()
+
+
+@allure.step('Check the visibility of the element')
+def should_see_issue_with_number(number):
+    browser.all('.js-issue-row').element_by(have.text(f'#{number}')).should(be.visible)
--- tests/test_github_issue_with_lambda_steps.py
+++ tests/test_github_issue_with_lambda_steps.py
@@ -13,7 +13,7 @@ def test_github_issue():
     with allure.step('Open repository\'s page by link from search result'):
         browser.element('[href="/KseniaPanfilova/QA_GURU_HW_8"]').click()
 
-    with allure.step('Go to issues page'):
+    with allure.step('Go to issues tab'):
         browser.element('#issues-tab').click()
 
     with allure.step('Check the visibility of the element'):
