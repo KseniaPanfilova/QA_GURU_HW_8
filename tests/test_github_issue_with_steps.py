38e1b11887611c773774d77f0f44493875c15135
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-09-04 22:01:04 +0300

    add test with lambda steps

--- tests/test_github_issue_with_steps.py
+++ tests/test_github_issue_with_steps.py
@@ -1,14 +1,20 @@
+import allure
 from selene import browser, have, be
 
 
 def test_github_issue():
-    browser.open('https://github.com')
+    with allure.step('Open GitHub main page'):
+        browser.open('https://github.com')
 
-    browser.element('.header-search-button').click()
-    browser.element('#query-builder-test').type('KseniaPanfilova/QA_GURU_HW_8').press_enter()
+    with allure.step('Look for repository'):
+        browser.element('.header-search-button').click()
+        browser.element('#query-builder-test').type('KseniaPanfilova/QA_GURU_HW_8').press_enter()
 
-    browser.element('[href="/KseniaPanfilova/QA_GURU_HW_8"]').click()
+    with allure.step('Open repository\'s page by link from search result'):
+        browser.element('[href="/KseniaPanfilova/QA_GURU_HW_8"]').click()
 
-    browser.element('#issues-tab').click()
+    with allure.step('Go to issues page'):
+        browser.element('#issues-tab').click()
 
-    browser.all('.js-issue-row').element_by(have.text('#1')).should(be.visible)
+    with allure.step('Check the visibility of the element'):
+        browser.all('.js-issue-row').element_by(have.text('#1')).should(be.visible)
