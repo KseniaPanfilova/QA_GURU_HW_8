99587e20245851b4ce66ebe8cff167e4a73496e8
Ksenia Panfilova <kseniapanfilova@gmail.com>
2024-09-04 21:50:55 +0300

    add test without steps

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ from selene import browser
 
 
 @pytest.fixture(scope="function", autouse=True)
-def window():
+def window_size():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
